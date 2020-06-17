8e37aadb10e2464a7a44b8a5d91c3f2178c2e446
asbyth <cyronize@gmail.com>
2020-06-17 14:34:33 -0400

    update version

--- src/main/java/club/sk1er/popupevents/PopupEvents.java
+++ src/main/java/club/sk1er/popupevents/PopupEvents.java
@@ -21,7 +21,7 @@ public class PopupEvents {
 
     public static final String MODID = "popup_events";
     public static final String NAME = "PopupEvents";
-    public static final String VERSION = "1.3";
+    public static final String VERSION = "1.3.1";
 
     private final ConfirmationPopup confirmationPopup = new ConfirmationPopup();
     private KeyBinding keybindAccept, keybindDeny;
