61d6e18bc4f19ffe88729bc7f66afe46d30ba691
asbyth <cyronize@gmail.com>
2021-05-25 07:39:38 -0400

    change: update loader & essential

--- src/main/java/club/sk1er/popupevents/tweaker/PopupEventsTweaker.java
+++ src/main/java/club/sk1er/popupevents/tweaker/PopupEventsTweaker.java
@@ -1,9 +1,9 @@
 package club.sk1er.popupevents.tweaker;
 
-import gg.essential.loader.EssentialTweaker;
+import gg.essential.loader.EssentialSetupTweaker;
 
 @SuppressWarnings("unused")
-public class PopupEventsTweaker extends EssentialTweaker {
+public class PopupEventsTweaker extends EssentialSetupTweaker {
     public PopupEventsTweaker() {
         super(new String[]{});
     }
